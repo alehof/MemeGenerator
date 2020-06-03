ce458d5de7104323d4ee4a107f2fcde08e85e293
alehof <alexander.hofmueller@gmail.com>
2020-06-03 12:25:23 +0200

    Added Basic DataBindingUtil Added Responsive Icons Added Layout Added Constraints
    //missing, bigger ICON sizes for buttons
    //Constraints not as planed in the layout

--- app/src/main/java/com/example/memegenerator/MainActivity.kt
+++ app/src/main/java/com/example/memegenerator/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.memegenerator
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.memegenerator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val view = binding.root
+        setContentView(view)
+        binding.titleText.text = "Edit textivew Text"
     }
 }
