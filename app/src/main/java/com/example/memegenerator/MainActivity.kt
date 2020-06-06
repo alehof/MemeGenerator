8fb51c277e5ddd21b32398a6119243d80c862ba6
alehof <alexander.hofmueller@gmail.com>
2020-06-06 14:36:23 +0200

    Revert "working on the cameraPart"
    This reverts commit c3aa934f5ffb7a817fe4456568693a842cd3f11b.
    
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/memegenerator/MainActivity.kt

--- app/src/main/java/com/example/memegenerator/MainActivity.kt
+++ app/src/main/java/com/example/memegenerator/MainActivity.kt
@@ -10,8 +10,6 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
-import android.support.annotation.RequiresApi
-import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
@@ -45,10 +43,6 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        button_camera.setOnClickListener{
-            prepTakePhoto()
-        }
-
 // CODE FROM https://devofandroid.blogspot.com/2018/09/pick-image-from-gallery-android-studio_15.html
        // For Retrieving Image from Gallery with Permission Checks.
         button_open.setOnClickListener {
@@ -72,29 +66,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-// See if we have permission or not
-    @RequiresApi(Build.VERSION_CODES.M)
-    private fun prepTakePhoto() {
-        if (ContextCompat.checkSelfPermission(this!!,Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-            takePhoto()
-        } else {
-            val permissionRequest = arrayOf(Manifest.permission.CAMERA) // aksing for camera permission, it can accept more than 1 permission at once that's why its in an array
-            requestPermissions(permissionRequest,CAMERA_PERMISSION_REQUEST_CODE)
-        }
-    }
-
-
-
-
-    private fun takePhoto() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        // on this new intent, also apply the things I stated
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also{
-            takePictureIntent -> takePictureIntent.resolveActivity(this!!.packageManager)?.also {
-            startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE)
-        }
-        }
-    }
 
     private fun pickImageFromGallery() {
         //Intent to pick image
@@ -108,10 +79,6 @@ class MainActivity : AppCompatActivity() {
         private val IMAGE_PICK_CODE = 1000;
         //Permission code
         private val PERMISSION_CODE = 1001;
-
-        private val CAMERA_PERMISSION_REQUEST_CODE = 2000;
-
-        private val CAMERA_REQUEST_CODE = 2001;
     }
 
     //handle requested permission result
@@ -126,15 +93,6 @@ class MainActivity : AppCompatActivity() {
                 else{
                     //permission from popup denied
                     Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
-                }
-            }
-
-            CAMERA_PERMISSION_REQUEST_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission granted, let's do stuff
-                    takePhoto()
-                } else {
-                    Toast.makeText(this, "Unable to take photo without permission",Toast.LENGTH_SHORT).show()
                 }
             }
         }
